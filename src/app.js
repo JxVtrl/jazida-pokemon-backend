d31b241d48b5b3c2b383187a73d2044282fbf10e
Joao Vitral <joaoviniciusvitral@hotmail.com>
2025-06-29 14:33:55 -0300

    feat: corrige domínio de homologação no CORS para o ambiente de desenvolvimento

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors({
     origin: [
         'https://jazida.pokemon.majorssolutions.com.br',
-        'https://jazida-hom.pokemon.majorssolutions.com.br',
+        'https://jazida.pokemon-hom.majorssolutions.com.br',
         'https://jazida.api.majorssolutions.com.br',
         'https://jazida-hom.api.majorssolutions.com.br',
         'https://jazida-pokemon-frontend.vercel.app',
